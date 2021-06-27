4bb340cfee5dc5672b8fb8530f210c0f98d10479
Abdurrahman Abul-Basher <arbasher@alumni.ubc.ca>
2021-06-27 13:21:46 -0700

    update files

--- src/utility/file_path.py
+++ src/utility/file_path.py
@@ -5,8 +5,6 @@ REPO_PATH = DIRECTORY_PATH.split(os.sep)
 REPO_PATH = os.sep.join(REPO_PATH[:-2])
 
 LOG_PATH = os.path.join(REPO_PATH, 'log')
-OBJECT_PATH = os.path.join(REPO_PATH, 'objectset')
 DATASET_PATH = os.path.join(REPO_PATH, 'dataset')
-INPUT_PATH = os.path.join(REPO_PATH, 'inputset')
 RESULT_PATH = os.path.join(REPO_PATH, 'result')
 MODEL_PATH = os.path.join(REPO_PATH, 'model')
